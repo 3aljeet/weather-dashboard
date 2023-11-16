b9ee1fe32b74d2c584d2ca7f25dc2c52b5068d1e
ytbarek <130870489+3aljeet@users.noreply.github.com>
2023-11-16 02:16:17 -0800

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,204 +1,45 @@
 
+const apiKey = 'd118763e9d5abf86d4999ca0364a8e00';
 
-// app.listen(port);
-// console.log('Server started at http://localhost:' + port);
+$(document).ready(function () {
+  $("#searchButton").click(function () {
+    const city = $("#cityInput").val();
+    getCurrentWeather(city);
+  });
 
-// const apiKey = 'd118763e9d5abf86d4999ca0364a8e00';
-
-// document.getElementById('searchButton').addEventListener('click', () => {
-//     const city = document.getElementById('cityInput').value;
-//     getCurrentWeather(city);
-//     get5DayForecast(city);
-//     addToSearchHistory(city);
-// });
-
-// function getCurrentWeather(city) {
-//     // Implement fetching and displaying current weather data here
-//     // Use the OpenWeatherMap API with the city name and apiKey
-// const apiKey = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey}';
-
-
-//     // Define the OpenWeatherMap API endpoint for current weather
-//     const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-
-//     // Make an HTTP GET request to the API
-//     fetch(apiUrl)
-//         .then(response => response.json())
-//         .then(data => {
-//             // Handle the current weather data here
-//             displayCurrentWeather(data);
-//         })
-//         .catch(error => {
-//             console.error('Error fetching current weather data:', error);
-//             // You can add error handling here, such as displaying an error message to the user
-//         });
-// }
-
-// function displayCurrentWeather(data) {
-//     // Extract and display relevant weather information for the current weather
-//     const cityName = data.name;
-//     const temperature = data.main.temp;
-//     const description = data.weather[0].description;
-
-//     // Display the current weather information in your HTML or console
-//     document.getElementById('currentWeatherCity').textContent = `Weather in ${cityName}:`;
-//     document.getElementById('currentTemperature').textContent = `Temperature: ${temperature}°C`;
-//     document.getElementById('currentDescription').textContent = `Description: ${description}`;
-// }
-
-// // Example usage
-// document.getElementById('searchButton').addEventListener('click', () => {
-//     const city = document.getElementById('cityInput').value;
-//     getCurrentWeather(city);
-    
-// });
-
-
-// function get5DayForecast(city) {
-   
-//         // Replace 'YOUR_API_KEY' with your actual OpenWeatherMap API key
-        
-    
-//         // Define the OpenWeatherMap API endpoint for 5-day forecast
-//         const apiUrl = `https://openweathermap.org/forecast5#5days`;
-    
-//         // Make an HTTP GET request to the API
-//         fetch(apiUrl)
-//             .then(response => response.json())
-//             .then(data => {
-//                 // Handle the 5-day forecast data here
-//                 display5DayForecast(data);
-//             })
-//             .catch(error => {
-//                 console.error('Error fetching 5-day forecast data:', error);
-//                 // You can add error handling here, such as displaying an error message to the user
-//             });
-    
-    
-//     function display5DayForecast(data) {
-//            // Ensure the forecast list is not empty
-//     if (!data.list || data.list.length === 0) {
-//         console.log('No 5-day forecast data available.');
-//         return;
-//     }
-
-//     // Extract and display relevant weather information for the 5-day forecast
-//     const city = data.city.name;
-
-//     console.log(`5-Day Forecast for ${city}:`);
-
-//     // Loop through the forecast list (typically contains data for every 3 hours)
-//     data.list.forEach(item => {
-//         // Extract relevant information for each forecast item
-//         const timestamp = new Date(item.dt * 1000); // Convert timestamp to a readable date
-//         const temperature = item.main.temp;
-//         const description = item.weather[0].description;
-
-//         // Log or display this information as needed
-//         console.log('Date:', timestamp.toDateString());
-//         console.log('Temperature:', temperature + '°C');
-//         console.log('Description:', description);
-//         console.log('---------------------------------------');
-//     });
-
-//         console.log('5-Day Forecast Data:', data);
-//     }
-    
-//     // Example usage
-//     document.getElementById('searchButton').addEventListener('click', () => {
-//         const city = document.getElementById('cityInput').value;
-//         getCurrentWeather(city);
-//         get5DayForecast(city); // Call the get5DayForecast function when the search button is clicked
-//         // ... You can also call addToSearchHistory(city) here.
-//     });
-    
-// }
-
-// function addToSearchHistory(city) {
-//  // Get the existing search history from local storage (if any)
-//  let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
-
-//  // Add the new city to the search history
-//  searchHistory.push(city);
-
-//  // Limit the search history to a certain number of items if needed
-//  const maxHistoryItems = 10; // Set your desired limit
-//  if (searchHistory.length > maxHistoryItems) {
-//      searchHistory = searchHistory.slice(-maxHistoryItems);
-//  }
-
-//  // Update the search history in local storage
-//  localStorage.setItem('searchHistory', JSON.stringify(searchHistory));
-
-//  // Update the display of the search history in the user interface
-//  displaySearchHistory(searchHistory);
-// }
-
-// function displaySearchHistory(searchHistory) {
-//  // Assuming you have an HTML element with the ID 'searchHistoryList' to display the search history
-//  const historyList = document.getElementById('searchHistoryList');
-
-//  // Clear the current content
-//  historyList.innerHTML = '';
-
-//  // Create list items for each city in the search history
-//  searchHistory.forEach(city => {
-//      const listItem = document.createElement('li');
-//      listItem.textContent = city;
-//      listItem.addEventListener('click', () => {
-//          // Handle click event when a search history item is clicked
-//          // For example, you can initiate a new search with the clicked city
-//          getCurrentWeather(city);
-//          get5DayForecast(city);
-//      });
-
-//      historyList.appendChild(listItem);
-//  });
-// }
-
-// // Example usage
-// document.getElementById('searchButton').addEventListener('click', () => {
-//  const city = document.getElementById('cityInput').value;
-//  getCurrentWeather(city);
-//  get5DayForecast(city);
-//  addToSearchHistory(city); // Add the city to the search history
-// });
-
-
-// // Additional functions for handling the search history click and error handling
-
-const apiKey = 'd118763e9d5abf86d4999ca0364a8e00'; 
-document.getElementById('searchButton').addEventListener('click', () => {
-    const city = document.getElementById('cityInput').value;
+  // Add click event for history items
+  $(document).on('click', '#searchHistoryList li', function () {
+    const city = $(this).text();
     getCurrentWeather(city);
     get5DayForecast(city);
-    addToSearchHistory(city);
+  });
 });
 
 function getCurrentWeather(city) {
-    // Define the OpenWeatherMap API endpoint for current weather
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
-    // Make an HTTP GET request to the API
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            displayCurrentWeather(data);
-        })
-        .catch(error => {
-            console.error('Error fetching current weather data:', error);
-        });
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      displayCurrentWeather(data);
+      get5DayForecast(city); // Move 5-day forecast call here to ensure it's only called after current weather is fetched
+      addToSearchHistory(city);
+    })
+    .catch(error => {
+      console.error('Error fetching current weather data:', error);
+    });
 }
 
 function displayCurrentWeather(data) {
-    const cityName = data.name;
-    const temperature = data.main.temp;
-    const description = data.weather[0].description;
+  const cityName = data.name;
+  const temperature = data.main.temp;
+  const description = data.weather[0].description;
 
-    document.getElementById('currentWeatherCity').textContent = `Weather in ${cityName}:`;
-    document.getElementById('currentTemperature').textContent = `Temperature: ${temperature}°C`;
-    document.getElementById('currentIcon').textContent = `Description: ${description}`; // Updated the ID to match an existing element
+  $('#currentWeatherCity').text(`Weather in ${cityName}:`);
+  $('#currentTemperature').text(`Temperature: ${temperature}°C`);
+  $('#currentIcon').text(`Description: ${description}`);
 }
+
 
 function get5DayForecast(city) {
     const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`; // Updated the API URL
@@ -214,26 +55,42 @@ function get5DayForecast(city) {
 }
 
 function display5DayForecast(data) {
+    const forecastList = $('#forecastList');
+    forecastList.empty(); // Clear previous forecast data
+  
     if (!data.list || data.list.length === 0) {
-        console.log('No 5-day forecast data available.');
-        return;
+      console.log('No 5-day forecast data available.');
+      return;
     }
-
+  
     const city = data.city.name;
     console.log(`5-Day Forecast for ${city}:`);
-
+  
+    let daysDisplayed = 0;
+  
     data.list.forEach(item => {
+      if (daysDisplayed < 5) {
         const timestamp = new Date(item.dt * 1000);
         const temperature = item.main.temp;
         const description = item.weather[0].description;
+        const wind = item.wind.speed;
 
-        console.log('Date:', timestamp.toDateString());
-        console.log('Temperature:', temperature + '°C');
-        console.log('Description:', description);
-        console.log('---------------------------------------');
+
+        const forecastDay = $('<div class="forecastgitDay"></div>');
+        forecastDay.append(`<p>Date: ${timestamp.toDateString()}</p>`);
+        forecastDay.append(`<p>Temperature: ${temperature}°C</p>`);
+        forecastDay.append(`<p>Description: ${description}</p>`);
+  
+        forecastList.append(forecastDay);
+  
+        daysDisplayed++;
+      } else {
+        // Exit the loop once 5 days are displayed
+        return false;
+      }
     });
-}
-
+  }
+  
 function addToSearchHistory(city) {
     let searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
 
@@ -266,9 +123,3 @@ function displaySearchHistory(searchHistory) {
     });
 }
 
-document.getElementById('searchButton').addEventListener('click', () => {
-    const city = document.getElementById('cityInput').value;
-    getCurrentWeather(city);
-    get5DayForecast(city);
-    addToSearchHistory(city);
-});
